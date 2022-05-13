1f8ccb0c7787e6c1c2c1e80e8c103028af68ab20
Coco62442 <corentinclement10@gmail.com>
2022-05-13 10:48:28 +0200

    maj graph

--- myapp/src/graph/Graph.java
+++ myapp/src/graph/Graph.java
@@ -4,7 +4,8 @@ public interface Graph {
 	public int nbOfVertices();
     public int nbOfEdges();
     public void addVertex(Vertex v1);
-    public void addEdge(Vertex v1,Vertex v2);
+    public void addEdge(Edge e);
+	public void addEdge(Vertex v1, Vertex v2, int source, double value, Color color);
     public boolean isConnected(Vertex v1,Vertex v2);
     public boolean isConnected();
     public Edge[] getEdges(Vertex v1, Vertex v2);
